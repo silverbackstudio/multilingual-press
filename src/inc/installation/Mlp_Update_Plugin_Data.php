66c0f1e02ad0754c134fae3db083fb250971cd44
tfrommen <tf@ipm-frommen.de>
2015-09-15 17:36:05 +0200

    Update Update class. #171

--- src/inc/installation/Mlp_Update_Plugin_Data.php
+++ src/inc/installation/Mlp_Update_Plugin_Data.php
@@ -1,16 +1,11 @@
-<?php
+<?php # -*- coding: utf-8 -*-
+
 /**
  * MultilingualPress Installation
- *
- * @version 2014.09.05
- * @author  Inpsyde GmbH, ChriCo, toscho
- * @license GPL
  */
 class Mlp_Update_Plugin_Data {
 
 	/**
-	 * Plugin data
-	 *
 	 * @var Inpsyde_Property_List_Interface
 	 */
 	private $plugin_data;
@@ -36,51 +31,59 @@ class Mlp_Update_Plugin_Data {
 	private $all_sites;
 
 	/**
-	 * Constructor
+	 * Constructor. Set up the properties.
 	 *
-	 * @param   Inpsyde_Property_List_Interface $plugin_data
-	 * @param   wpdb                            $wpdb
-	 * @param   Mlp_Version_Number_Interface    $current_version
-	 * @param   Mlp_Version_Number_Interface    $last_version
+	 * @param Inpsyde_Property_List_Interface $plugin_data
+	 * @param wpdb                            $wpdb
+	 * @param Mlp_Version_Number_Interface    $current_version
+	 * @param Mlp_Version_Number_Interface    $last_version
+	 *
 	 * @return  Mlp_Update_Plugin_Data
 	 */
 	public function __construct(
 		Inpsyde_Property_List_Interface $plugin_data,
-		wpdb                            $wpdb,
-		Mlp_Version_Number_Interface    $current_version,
-		Mlp_Version_Number_Interface    $last_version
+		wpdb $wpdb,
+		Mlp_Version_Number_Interface $current_version,
+		Mlp_Version_Number_Interface $last_version
 	) {
 
-		$this->plugin_data     = $plugin_data;
-		$this->last_version    = $last_version;
+		$this->plugin_data = $plugin_data;
+
+		$this->wpdb = $wpdb;
+
 		$this->current_version = $current_version;
-		$this->wpdb            = $wpdb;
-		$this->all_sites       = wp_get_sites();
+
+		$this->last_version = $last_version;
+
+		$this->all_sites = wp_get_sites();
 	}
 
 	/**
-	 * Handles the update routines.
+	 * Handle the update routines.
 	 *
 	 * @param Mlp_Network_Plugin_Deactivation_Interface $deactivator
+	 *
 	 * @return void
 	 */
 	public function update( Mlp_Network_Plugin_Deactivation_Interface $deactivator ) {
 
 		$deactivator->deactivate(
-			array (
+			array(
 				'disable-acf.php',
-				'mlp-wp-seo-compat.php'
+				'mlp-wp-seo-compat.php',
 			)
 		);
-		// add hook to import active languages when reset is done
-		add_action( 'mlp_reset_table_done', array ( $this, 'import_active_languages' ) );
 
-		// The site option with the version number exists since 2.0. If the last
-		// version is a fallback, it is a version below 2.0.
-		if ( Mlp_Version_Number_Interface::FALLBACK_VERSION === $this->last_version )
+		// add hook to import active languages when reset is done
+		add_action( 'mlp_reset_table_done', array( $this, 'import_active_languages' ) );
+
+		// The site option with the version number exists since 2.0.
+		// If the last version is a fallback, it is a version below 2.0.
+		if ( Mlp_Version_Number_Interface::FALLBACK_VERSION === $this->last_version ) {
 			$this->update_plugin_data( 1 );
-		else
+		} else {
 			$this->update_plugin_data( $this->last_version );
+		}
 	}
 
 	/**
@@ -96,11 +99,22 @@ class Mlp_Update_Plugin_Data {
 			$this->import_active_languages( new Mlp_Db_Languages_Schema( $this->wpdb ) );
 		}
 
-		if ( version_compare( $last_version, '2.0.4', '<' ) ) {
-			$installer = new Mlp_Db_Installer( new Mlp_Site_Relations_Schema( $this->wpdb ) );
-			$installer->install();
-			$this->import_site_relations();
-			$this->update_type_column( new Mlp_Content_Relations_Schema( $this->wpdb ) );
+		// TODO: Define correct version
+		if ( version_compare( $last_version, '2.3.0', '<' ) ) {
+
+			$installer = new Mlp_Db_Installer( $this->plugin_data->get( 'site_relations_schema' ) );
+
+			if ( version_compare( $last_version, '2.0.4', '<' ) ) {
+				$installer->install();
+
+				$this->import_site_relations();
+
+				$this->update_type_column();
+			}
+
+			$installer->install( $this->plugin_data->get( 'relationships_schema' ) );
+
+			$installer->install( $this->plugin_data->get( 'content_relations_schema' ) );
 		}
 
 		// remove unneeded plugin data
@@ -116,10 +130,12 @@ class Mlp_Update_Plugin_Data {
 	 */
 	private function import_site_relations() {
 
+		$option_name = 'inpsyde_multilingual_blog_relationship';
+
+		$inserted = 0;
+
 		/** @var Mlp_Site_Relations_Interface $db */
 		$relations = $this->plugin_data->get( 'site_relations' );
-		$option_name = 'inpsyde_multilingual_blog_relationship';
-		$inserted = 0;
 
 		foreach ( $this->all_sites as $site ) {
 
@@ -134,68 +150,76 @@ class Mlp_Update_Plugin_Data {
 	}
 
 	/**
-	 * Update mlp_multilingual_linked table and set type to "post" if empty
+	 * Update mlp_multilingual_linked table and set type to "post" if empty.
 	 *
-	 * @param Mlp_Db_Schema_Interface $linked
 	 * @return void
 	 */
-	private function update_type_column( Mlp_Db_Schema_Interface $linked ) {
+	private function update_type_column() {
 
-		$table = $linked->get_table_name();
-		$this->wpdb->query(
-			'UPDATE ' . $table . ' set ml_type = "post" WHERE ml_type != "term"'
-		);
+		$sql = "
+UPDATE {$this->wpdb->base_prefix}multilingual_linked
+SET ml_type = 'post'
+WHERE ml_type != 'term'";
+
+		$this->wpdb->query( $sql );
 	}
 
 	/**
-	 * Load the localization
+	 * Load the localization.
 	 *
-	 * @since 0.1
-	 * @uses load_plugin_textdomain, plugin_basename
 	 * @param Mlp_Db_Schema_Interface $languages
+	 *
 	 * @return void
 	 */
 	private function import_active_languages( Mlp_Db_Schema_Interface $languages ) {
 
 		// get active languages
 		$mlp_settings = get_site_option( 'inpsyde_multilingual' );
-
-		if ( empty ( $mlp_settings ) )
+		if ( empty( $mlp_settings ) ) {
 			return;
+		}
 
 		$table = $languages->get_table_name();
-		$sql   = 'SELECT ID FROM ' . $table . 'WHERE wp_locale = %s OR iso_639_1 = %s';
+
+		$sql = "
+SELECT ID
+FROM $table
+WHERE wp_locale = %s
+	OR iso_639_1 = %s";
 
 		foreach ( $mlp_settings as $mlp_site ) {
-			$text    = $mlp_site[ 'text' ] != '' ? $mlp_site[ 'text' ] : $mlp_site[ 'lang' ];
-			$query   = $this->wpdb->prepare( $sql, $mlp_site[ 'lang' ], $mlp_site[ 'lang' ] );
-			$lang_id = $this->wpdb->get_var( $query );
 
-			// language not found -> insert
-			if ( empty ( $lang_id ) ) {
+			$query = $this->wpdb->prepare( $sql, $mlp_site[ 'lang' ], $mlp_site[ 'lang' ] );
+
+			$lang_id = $this->wpdb->get_var( $query );
+			if ( empty( $lang_id ) ) {
+				$text = $mlp_site[ 'text' ] !== ''
+					? $mlp_site[ 'text' ]
+					: $mlp_site[ 'lang' ];
+
+				// language not found -> insert
 				// @todo add custom name
 				$this->wpdb->insert(
 					$table,
-				   array (
-					   'english_name' => $text,
-					   'wp_locale'    => $mlp_site[ 'lang' ],
-					   'http_name'    => str_replace( '_', '-', $mlp_site[ 'lang' ] )
-				   )
+					array(
+						'english_name' => $text,
+						'wp_locale'    => $mlp_site[ 'lang' ],
+						'http_name'    => str_replace( '_', '-', $mlp_site[ 'lang' ] ),
+					)
 				);
-			}
-			// language found -> change priority
-			else {
+			} else {
+				// language found -> change priority
 				$this->wpdb->update(
 					$table,
-					array ( 'priority' => 10 ),
-					array ( 'ID'       => $lang_id )
+					array( 'priority' => 10 ),
+					array( 'ID' => $lang_id )
 				);
 			}
 		}
 	}
 
 	/**
-	 * Install plugin tables.
+	 * Install the plugin tables.
 	 *
 	 * @return bool
 	 */
@@ -203,8 +227,9 @@ class Mlp_Update_Plugin_Data {
 
 		$installer = new Mlp_Db_Installer( new Mlp_Db_Languages_Schema( $this->wpdb ) );
 		$installer->install();
-		$installer->install( new Mlp_Content_Relations_Schema( $this->wpdb ) );
-		$installer->install( new Mlp_Site_Relations_Schema( $this->wpdb ) );
+		$installer->install( $this->plugin_data->get( 'site_relations_schema' ) );
+		$installer->install( $this->plugin_data->get( 'relationships_schema' ) );
+		$installer->install( $this->plugin_data->get( 'content_relations_schema' ) );
 
 		return update_site_option( 'mlp_version', $this->plugin_data->get( 'version' ) );
 	}
