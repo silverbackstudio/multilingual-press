0f340de41646da0bc9a6cade994f1914ed1b1056
tfrommen <tf@ipm-frommen.de>
2015-10-01 18:44:31 +0200

    Prepare migration: Delete invalid content relations. #171

--- src/inc/installation/Mlp_Update_Plugin_Data.php
+++ src/inc/installation/Mlp_Update_Plugin_Data.php
@@ -102,7 +102,7 @@ class Mlp_Update_Plugin_Data {
 		// TODO: Define correct version
 		if ( version_compare( $last_version, '2.3.0', '<' ) ) {
 
-			// TODO: Delete all entries in the multilingual_linked (!) table for posts/terms that don't exist anymore
+			$this->delete_invalid_content_relations();
 
 			// TODO: Trigger data migration from multilingual_linked to both content_relations and relationships
 
@@ -125,6 +125,161 @@ class Mlp_Update_Plugin_Data {
 		delete_option( 'inpsyde_companyname' );
 
 		return update_site_option( 'mlp_version', $this->plugin_data->get( 'version' ) );
+	}
+
+	/**
+	 * Load the localization.
+	 *
+	 * @param Mlp_Db_Schema_Interface $languages
+	 *
+	 * @return void
+	 */
+	private function import_active_languages( Mlp_Db_Schema_Interface $languages ) {
+
+		// get active languages
+		$mlp_settings = get_site_option( 'inpsyde_multilingual' );
+		if ( empty( $mlp_settings ) ) {
+			return;
+		}
+
+		$table = $languages->get_table_name();
+
+		$query = "
+SELECT ID
+FROM $table
+WHERE wp_locale = %s
+	OR iso_639_1 = %s";
+
+		foreach ( $mlp_settings as $mlp_site ) {
+			$query = $this->wpdb->prepare( $query, $mlp_site[ 'lang' ], $mlp_site[ 'lang' ] );
+
+			$lang_id = $this->wpdb->get_var( $query );
+			if ( empty( $lang_id ) ) {
+				$text = $mlp_site[ 'text' ] !== ''
+					? $mlp_site[ 'text' ]
+					: $mlp_site[ 'lang' ];
+
+				// language not found -> insert
+				// @todo add custom name
+				$this->wpdb->insert(
+					$table,
+					array(
+						'english_name' => $text,
+						'wp_locale'    => $mlp_site[ 'lang' ],
+						'http_name'    => str_replace( '_', '-', $mlp_site[ 'lang' ] ),
+					)
+				);
+			} else {
+				// language found -> change priority
+				$this->wpdb->update(
+					$table,
+					array( 'priority' => 10 ),
+					array( 'ID' => $lang_id )
+				);
+			}
+		}
+	}
+
+	/**
+	 * Deletes all entries in the old multilingual_linked table for posts/terms that don't exist anmore.
+	 *
+	 * @return void
+	 */
+	private function delete_invalid_content_relations() {
+
+		$delete_query = "
+DELETE
+FROM {$this->wpdb->base_prefix}multilingual_linked
+WHERE ( ml_source_blogid = %d AND ml_source_elementid NOT IN (%s)
+		OR ml_blogid = %d AND ml_elementid NOT IN (%s)
+	)";
+
+		$delete_all_query = "
+DELETE
+FROM {$this->wpdb->base_prefix}multilingual_linked
+WHERE ml_source_blogid = %d
+	OR ml_blogid = %d";
+
+		foreach ( $this->all_sites as $site ) {
+			if ( empty( $site[ 'blog_id' ] ) ) {
+				continue;
+			}
+
+			$site_id = $site[ 'blog_id' ];
+
+			switch_to_blog( $site_id );
+
+			// Delete all entries for POSTS that don't exist anymore
+			$post_ids = $this->wpdb->get_results( "SELECT ID FROM {$this->wpdb->posts}", ARRAY_N );
+			if ( $post_ids ) {
+				$post_ids_string = '';
+
+				foreach ( $post_ids as $post_id ) {
+					$post_ids_string .= ',' . reset( $post_id );
+				}
+
+				$post_ids_string = substr( $post_ids_string, 1 );
+
+				$query = $this->wpdb->prepare(
+					$delete_query,
+					array(
+						$site_id,
+						$post_ids_string,
+						$site_id,
+						$post_ids_string,
+					)
+				);
+			} else {
+				$query = $this->wpdb->prepare(
+					$delete_all_query,
+					array(
+						$site_id,
+						$site_id,
+					)
+				);
+			}
+			$this->wpdb->query( "$query\n\tAND ml_type != 'term'" );
+
+			// Delete entries for TERMS that don't exist anymore
+			$term_taxonomy_ids = $this->wpdb->get_results(
+				"SELECT term_taxonomy_id FROM {$this->wpdb->term_taxonomy}",
+				ARRAY_N
+			);
+			if ( $term_taxonomy_ids ) {
+				$term_taxonomy_ids_string = '';
+
+				foreach ( $term_taxonomy_ids as $term_taxonomy_id ) {
+					$term_taxonomy_ids_string .= ',' . reset( $term_taxonomy_id );
+				}
+
+				$term_taxonomy_ids_string = substr( $term_taxonomy_ids_string, 1 );
+
+				$query = $this->wpdb->prepare(
+					$delete_query,
+					array(
+						$site_id,
+						$term_taxonomy_ids_string,
+						$site_id,
+						$term_taxonomy_ids_string,
+					)
+				);
+			} else {
+				$query = $this->wpdb->prepare(
+					$delete_all_query,
+					array(
+						$site_id,
+						$site_id,
+					)
+				);
+			}
+			$this->wpdb->query( "$query\n\tAND ml_type = 'term'" );
+		}
+
+		if ( ! empty( $GLOBALS[ '_wp_switched_stack' ] ) ) {
+			$GLOBALS[ '_wp_switched_stack' ] = (array) reset( $GLOBALS[ '_wp_switched_stack' ] );
+		}
+
+		restore_current_blog();
 	}
 
 	/**
@@ -160,66 +315,12 @@ class Mlp_Update_Plugin_Data {
 	 */
 	private function update_type_column() {
 
-		$sql = "
+		$query = "
 UPDATE {$this->wpdb->base_prefix}multilingual_linked
 SET ml_type = 'post'
-WHERE ml_type != 'term'";
+WHERE ml_type NOT IN('post','term')";
 
-		$this->wpdb->query( $sql );
-	}
-
-	/**
-	 * Load the localization.
-	 *
-	 * @param Mlp_Db_Schema_Interface $languages
-	 *
-	 * @return void
-	 */
-	private function import_active_languages( Mlp_Db_Schema_Interface $languages ) {
-
-		// get active languages
-		$mlp_settings = get_site_option( 'inpsyde_multilingual' );
-		if ( empty( $mlp_settings ) ) {
-			return;
-		}
-
-		$table = $languages->get_table_name();
-
-		$sql = "
-SELECT ID
-FROM $table
-WHERE wp_locale = %s
-	OR iso_639_1 = %s";
-
-		foreach ( $mlp_settings as $mlp_site ) {
-
-			$query = $this->wpdb->prepare( $sql, $mlp_site[ 'lang' ], $mlp_site[ 'lang' ] );
-
-			$lang_id = $this->wpdb->get_var( $query );
-			if ( empty( $lang_id ) ) {
-				$text = $mlp_site[ 'text' ] !== ''
-					? $mlp_site[ 'text' ]
-					: $mlp_site[ 'lang' ];
-
-				// language not found -> insert
-				// @todo add custom name
-				$this->wpdb->insert(
-					$table,
-					array(
-						'english_name' => $text,
-						'wp_locale'    => $mlp_site[ 'lang' ],
-						'http_name'    => str_replace( '_', '-', $mlp_site[ 'lang' ] ),
-					)
-				);
-			} else {
-				// language found -> change priority
-				$this->wpdb->update(
-					$table,
-					array( 'priority' => 10 ),
-					array( 'ID' => $lang_id )
-				);
-			}
-		}
+		$this->wpdb->query( $query );
 	}
 
 	/**
