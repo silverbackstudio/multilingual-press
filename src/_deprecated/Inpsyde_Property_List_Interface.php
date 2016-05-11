ec6f013a7000e57d38b075b92f6374a6e64f3591
tfrommen <info@tfrommen.de>
2016-05-11 08:24:35 +0200

    Add missing (deprecated) methods and adapt deprecation notice. #206

--- src/_deprecated/Inpsyde_Property_List_Interface.php
+++ src/_deprecated/Inpsyde_Property_List_Interface.php
@@ -1,12 +1,36 @@
 <?php # -*- coding: utf-8 -*-
 
-interface Inpsyde_Property_List_Interface extends Inpsyde\MultilingualPress\Common\Properties {
+use Inpsyde\MultilingualPress\Common\Properties;
 
+_deprecated_file( 'Inpsyde_Property_List_Interface', '3.0.0', 'Inpsyde\MultilingualPress\Common\Properties' );
+
+interface Inpsyde_Property_List_Interface extends Properties {
+
+	/**
+	 * Wrapper for set().
+	 *
+	 * @param string $name  Property name.
+	 * @param mixed  $value The new value.
+	 *
+	 * @return Properties|WP_Error
+	 */
+	public function __set( $name, $value );
+
+	/**
+	 * Wrapper for get().
+	 *
+	 * @param string $name Property name.
+	 *
+	 * @return mixed|null
+	 */
+	public function __get( $name );
+
+	/**
+	 * Wrapper for has().
+	 *
+	 * @param string $name Property name.
+	 *
+	 * @return bool
+	 */
+	public function __isset( $name );
 }
-
-_deprecated_file(
-	__FILE__,
-	'3.0.0',
-	'Inpsyde\MultilingualPress\Common\Properties',
-	'Please use the new interface.'
-);
